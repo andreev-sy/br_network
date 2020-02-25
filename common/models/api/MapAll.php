450dabb6061cb397d9cf2ba860d2a29f7e9828a5
MaKonst <solovyev@liderpoiska.ru>
2020-02-25 14:14:09 +0400

    add api

---
+++ common/models/api/MapAll.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace common\models\api;
+
+use yii\base\BaseObject;
+use common\models\Restaurants;
+use yii\helpers\ArrayHelper;
+use Yii;
+
+class MapAll extends BaseObject{
+
+	public $coords;
+
+	public function __construct() {
+
+		$restaurants = Restaurants::find()->all();
+		$this->coords = ArrayHelper::toArray($restaurants, [
+		    'common\models\Restaurants' => [
+		        'id',
+		        'latitude',
+		        'longitude'
+		    ],
+		]);
+		
+	}
+
+}
