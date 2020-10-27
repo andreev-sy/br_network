e91f4746bedd0856330b03c30964ef040f18b0ad
dkornilove <norz.qq@gmail.com>
2020-10-27 23:21:38 +0400

    edit README

--- backend/blog-constructor/settings_.js
+++ backend/blog-constructor/settings_.js
@@ -9,10 +9,10 @@ export const BLOCK_REL_FIELD = 'blog_block_id';
 export const BLOCK_REL_NAME = 'blogBlock';
 
 export const GET_BLOCKS =
-    '/api/blog-blocks/?fields=id,name,type&expand=blockTypeLabel';
-export const API_DRAFT_BLOCKS = '/api/blog-post-blocks/';
+    '/blog-blocks/?fields=id,name,type&expand=blockTypeLabel';
+export const API_DRAFT_BLOCKS = '/blog-post-blocks/';
 export const GET_DRAFT_BLOCKS = `${API_DRAFT_BLOCKS}?expand=${BLOCK_REL_NAME},mediaTargets`;
-export const POST_BLOCKS_SORTINGS = '/api/blog-post-blocks/sort/';
+export const POST_BLOCKS_SORTINGS = '/blog-post-blocks/sort/';
 
 export const POST_FILE = '/media/upload/';
 export const POST_FILE_FASTVIEW = '/media/fast-view/';
@@ -23,7 +23,7 @@ export const BLOCK_SHARED_SETTINGS = [
         slug: 'size',
         type: 'select',
         variants: [
-            { value: 's', label: 'Маленький', default: true },
+            { value: 's', label: 'Маленький', default: 1 },
             { value: 'm', label: 'Обычный' },
             { value: 'l', label: 'Большой' },
         ],
@@ -33,7 +33,7 @@ export const BLOCK_SHARED_SETTINGS = [
         slug: 'color',
         type: 'select',
         variants: [
-            { value: 'default', label: 'Стандартный', default: true },
+            { value: 'default', label: 'Стандартный', default: 1 },
             { value: 'blue', label: 'Синий' },
             { value: 'yellow', label: 'Желтый' },
             { value: 'gray', label: 'Серый' },
@@ -44,7 +44,7 @@ export const BLOCK_SHARED_SETTINGS = [
         slug: 'margin',
         type: 'select',
         variants: [
-            { value: 'default', label: 'Обычный', default: true },
+            { value: 'default', label: 'Обычный', default: 1 },
             { value: 'large', label: 'Большой' },
             { value: 'nomargin', label: 'Без отступов' },
         ],
