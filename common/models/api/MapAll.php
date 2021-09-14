72fa940c53133a0db19d40f1ce830363b29ccd9a
Martynov Konstantin <martynov@liderpoiska.ru>
2021-09-14 10:11:17 +0200

    pre-dev

--- common/models/api/MapAll.php
+++ common/models/api/MapAll.php
@@ -8,11 +8,13 @@ use yii\helpers\ArrayHelper;
 use common\models\Restaurants;
 use common\models\elastic\ItemsFilterElastic;
 
-class MapAll extends BaseObject{
+class MapAll extends BaseObject
+{
 
 	public $coords;
 
-	public function __construct($elastic_model, $subdomain_id, $filter = [], $type = 'restaurants', $url = '/ploshhadki/', $link_type = 'id') {
+	public function __construct($elastic_model, $subdomain_id, $filter = [], $type = 'restaurants', $url = '/ploshhadki/', $link_type = 'id')
+	{
 
 		$items = new ItemsFilterElastic($filter, 9000, 1, false, $type, $elastic_model, false, false, $subdomain_id);
 		$this->coords = [
@@ -33,24 +35,35 @@ class MapAll extends BaseObject{
 						break;
 					}
 					isset($item->restaurant_unique_id) ? $restaurant_unique_id = $item->restaurant_unique_id : $restaurant_unique_id = null;
+
+					$lowest_price = $item->restaurant_price;
+					foreach ($item->rooms as $key => $room) {
+						if ($room['price'] < $lowest_price) {
+							$lowest_price = $room['price'];
+							break;
+						}
+					}
+
 					array_push($this->coords['features'], [
 						'type' => "Feature",
-			            'id' => $item->id,
-			            'unique_id' => $restaurant_unique_id,
-			            'geometry' => [
-			              'type' => "Point",
-			              'coordinates' => [$item->restaurant_latitude, $item->restaurant_longitude]
-			            ],
-			            'properties' => [
-			              'balloonContent' => $item->restaurant_address,
-			              'organization' => $item->restaurant_name,
-						  'type' => $rest_type,
-			              'address' => $item->restaurant_address,
-			              'img' => $map_preview,
-			              'clusterCaption' => $item->restaurant_name,
-			              'link' => $link_type == 'id' ? $url.$item->id.'/' : $url.$item->restaurant_slug.'/',
-			              'link_unique' => $link_type == 'id' ? $url.$item->restaurant_unique_id.'/' : $url.$item->restaurant_slug.'/',
-			            ]
+						'id' => $item->id,
+						'unique_id' => $restaurant_unique_id,
+						'geometry' => [
+							'type' => "Point",
+							'coordinates' => [$item->restaurant_latitude, $item->restaurant_longitude]
+						],
+						'properties' => [
+							'balloonContent' => $item->restaurant_address,
+							'organization' => $item->restaurant_name,
+							'type' => $rest_type,
+							'address' => $item->restaurant_address,
+							'img' => $map_preview,
+							'clusterCaption' => $item->restaurant_name,
+							'link' => $link_type == 'id' ? $url . $item->id . '/' : $url . $item->restaurant_slug . '/',
+							'link_unique' => $link_type == 'id' ? $url . $item->restaurant_unique_id . '/' : $url . $item->restaurant_slug . '/',
+							'lowestPrice' => $lowest_price,
+							'capacity' => $item->restaurant_max_capacity,
+						]
 					]);
 					break;
 				case 'rooms':
@@ -61,24 +74,24 @@ class MapAll extends BaseObject{
 					isset($item->restaurant_unique_id) ? $restaurant_unique_id = $item->restaurant_unique_id : $restaurant_unique_id = null;
 					array_push($this->coords['features'], [
 						'type' => "Feature",
-			            'id' => $item->id,
-			            'unique_id' => $restaurant_unique_id,
-			            'geometry' => [
-			              'type' => "Point",
-			              'coordinates' => [$item->restaurant_latitude, $item->restaurant_longitude]
-			            ],
-			            'properties' => [
-			              'balloonContent' => $item->restaurant_address,
-			              'organization' => $item->restaurant_name.', '.$item->name,
-			              'address' => $item->restaurant_address,
-			              'img' => $map_preview,
-			              'clusterCaption' => $item->name,
-			              'link' => '/catalog/'.$item->id.'/',
-			              'link_unique' => $link_type == 'id' ? $url.$item->unique_id.'/' : $url.$item->restaurant_slug.'/',
-			            ]
+						'id' => $item->id,
+						'unique_id' => $restaurant_unique_id,
+						'geometry' => [
+							'type' => "Point",
+							'coordinates' => [$item->restaurant_latitude, $item->restaurant_longitude]
+						],
+						'properties' => [
+							'balloonContent' => $item->restaurant_address,
+							'organization' => $item->restaurant_name . ', ' . $item->name,
+							'address' => $item->restaurant_address,
+							'img' => $map_preview,
+							'clusterCaption' => $item->name,
+							'link' => '/catalog/' . $item->id . '/',
+							'link_unique' => $link_type == 'id' ? $url . $item->unique_id . '/' : $url . $item->restaurant_slug . '/',
+						]
 					]);
 					break;
-			}			
-		}		
+			}
+		}
 	}
 }
